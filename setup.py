eed59e40965d476f5df0d2a5beb5010e9caafa02
Lex Toumbourou <lextoumbourou@gmail.com>
2014-02-19 23:38:58 +1100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='PyWit',
+      version='0.1.0',
+      author='Lex Toumbourou',
+      author_email='lextoumbourou@gmail.com',
+      description='Python bindings for the Wit HTTP API',
+      license='MIT',
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Topic :: Utilities',
+          'Programming Language :: Python',
+          'Development Status :: 3 - Alpha',
+          'Environment :: Web Environment'
+      ],
+      packages=['wit'],
+      package_dir={'wit': 'wit'},
+      url='https://github.com/lextoumbourou/PyWit',
+      install_requires=['requests']
+)
