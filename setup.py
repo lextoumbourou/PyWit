44cbfa409b0ba54e74816d2f8721be896421fa46
Lex Toumbourou <lextoumbourou@gmail.com>
2014-02-21 08:35:56 +1100

    Version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 
 setup(name='PyWit',
-      version='0.1.0',
+      version='0.1.1',
       author='Lex Toumbourou',
       author_email='lextoumbourou@gmail.com',
       description='Python bindings for the Wit HTTP API',
